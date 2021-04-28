3dd0c3bea64cfb3eb5ac40e14ceeae9ca24883dd
Paul Hsueh <hsueh.paul@gmail.com>
2021-04-28 11:54:35 -0400

    commit tree eae78918e4f9441d92fa1b9c0c7c752988d94334

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    return Math.abs(42 - street);
+}
+
+function distanceFromHqInFeet(street) {
+    return distanceFromHqInBlocks(street) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs(start - end) * 264; 
+}
+
+function calculatesFarePrice(start, end) {
+    if (distanceTravelledInFeet(start, end) < 400) {
+        return 0; 
+    } else if (distanceTravelledInFeet(start, end) > 400 && distanceTravelledInFeet(start, end) < 2000) {
+        return (distanceTravelledInFeet(start, end) - 400) * 0.02; 
+    } else if (distanceTravelledInFeet(start, end) > 2000 && distanceTravelledInFeet(start, end) < 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far'; 
+    }
+
+}
